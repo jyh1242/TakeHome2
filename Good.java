460f8f215026b08df79649aabe30e256ac633556
yh <jyh1242@hanmail.net>
2017-12-13 17:07:35 +0900

    commit함

--- Good.java
+++ Good.java
@@ -5,7 +5,7 @@ public static void main(String[] args)
 for(int i=1;i<=10;i++)
 {
 System.out.println("Good Sun..."+i);
-System.out.println("Goo...!!"+i);
+System.out.println("Goo...!!22"+i);
 }
 }
 }
